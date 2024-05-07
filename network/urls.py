e3e561440929b9ece75e902c3ea4473a41a59149
hon-nova <hon.tnguyen294@gmail.com>
2024-05-06 22:26:37 -0700

    added following

--- network/urls.py
+++ network/urls.py
@@ -11,5 +11,7 @@ urlpatterns = [
     path('posts',views.save_post,name="posts"),
     path('profile/<int:poster_id>',views.profile,name="profile"),
     path('likes/<int:post_id>',views.toggle_like,name="likes"),
-    path('followers/<str:poster_username>',views.save_follower,name="followers")
+    path('followers/<str:poster_username>',views.save_follower,name="followers"),
+    path('following',views.following,name="following"),
+    path('save_content/<int:post_id>',views.save_content,name="save_content")
 ]
--- network/views.py
+++ network/views.py
@@ -95,12 +95,10 @@ def profile(request,poster_id):
         # logging.debug(f'all_user_posts::{all_user_posts}')
         number_of_my_followers=this_user.followers.count()
         # following_users
-        logging.debug(f'num_of_my_followers::{number_of_my_followers}')
+        # logging.debug(f'num_of_my_followers::{number_of_my_followers}')
         number_of_my_following=this_user.following_users.count()
-        logging.debug(f'number_of_my_following::{number_of_my_following}')
-     
+        # logging.debug(f'number_of_my_following::{number_of_my_following}')  
        
-        # MUST KNOW user must log in to display Follow or Unfollow btn
         return render(request,'network/profile.html',{'posts':all_user_posts,'num_followers':number_of_my_followers,'num_following':number_of_my_following,'poster':this_user})
     
     else:
@@ -144,17 +142,44 @@ def save_follower(request,poster_username):
             is_already_following=request.user.following_users.filter(followed=poster).exists()
             
             if not is_already_following: 
-                follower=Follower(follower=request.user,followed=poster)                            
-               
-                follower.is_followed=True
+                follower=Follower(follower=request.user,followed=poster)
                 follower.save()
             else:
                 follower=request.user.following_users.filter(followed=poster) 
-                follower.delete()  
-                
-            return HttpResponseRedirect(reverse('profile',args=[poster.id]))
+                follower.delete() 
+            is_following=request.user.following_users.filter(followed=poster).exists()
+            logging.debug(f'is_following::{is_following}')
+            return HttpResponseRedirect(reverse('profile',args=[poster.id]) + f'?is_following={is_following}')
+           
     else:
         return redirect('login')
+    
+def following(request):
+    if request.user.is_authenticated:
+        # all_users_i_follow=request.user.following_users.all()
+        #  Retrieve all users that the current user is following
+        all_users_i_follow = User.objects.filter(followers__follower=request.user)
+        all_users_posts=[]
+        
+        all_users_posts=Post.objects.filter(poster__in=all_users_i_follow)
+       
+        all_users_posts=all_users_posts.order_by('-timestamp')
+        logging.debug(f'all_users_posts::{all_users_posts}')
+    return render(request,'network/following.html',{'posts':all_users_posts})
+
+def save_content(request,post_id):
+    if request.method=="POST":
+        content=request.POST.get('content')
+        post=get_object_or_404(Post,pk=post_id)
+        if content:
+            post.content=content
+            post.save()
+            
+    return HttpResponseRedirect(reverse('index'))
+        
+        
+    
+    
     
     
         
